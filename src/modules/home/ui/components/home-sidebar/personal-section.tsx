d9338f034f87ff4523b223cdfb624fa45c480d71
VerticalHost <100486259+verticalhost@users.noreply.github.com>
2025-03-17 14:35:53 -0400

    MISE A JOURS

--- src/modules/home/ui/components/home-sidebar/personal-section.tsx
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useAuth, useClerk } from "@clerk/nextjs";
-import { HistoryIcon, ListVideoIcon, ThumbsUpIcon, FileTextIcon } from "lucide-react";
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
 
 import { 
   SidebarGroup, 
@@ -30,12 +32,6 @@ const items = [
     url: "/playlists",
     icon: ListVideoIcon,
     auth: true,
-  },
-  {
-    title: "Terms",
-    url: "/terms",
-    icon: FileTextIcon,
-    auth: false,
   },
 ];
 
